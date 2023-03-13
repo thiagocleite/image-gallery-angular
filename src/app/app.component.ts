184798daab8ef0956d3727622d1cf55569d0e090
Thiago Leite <thiagoleite@MacBook-Pro-de-Thiago.local>
2023-03-13 00:26:51 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'image-gallery';
+export class AppComponent implements OnInit {
+  public imgs = [
+    {
+      id: 1,
+      img: 'https://source.unsplash.com/VWcPlbHglYc',
+    },
+    {
+      id: 2,
+      img: 'https://source.unsplash.com/e6FMMambeO4',
+    },
+    {
+      id: 3,
+      img: 'https://source.unsplash.com/klCiPmzUw0Y',
+    },
+    {
+      id: 4,
+      img: 'https://source.unsplash.com/IdNOTjPeHrE',
+    },
+    {
+      id: 5,
+      img: 'https://source.unsplash.com/O0N9MF--hK4',
+    },
+    {
+      id: 6,
+      img: 'https://source.unsplash.com/FV3GConVSss',
+    },
+    {
+      id: 7,
+      img: 'https://source.unsplash.com/0ESjL-Nw22Y',
+    },
+    {
+      id: 8,
+      img: 'https://source.unsplash.com/KTVn62x6fFw',
+    },
+    {
+      id: 9,
+      img: 'https://source.unsplash.com/VSeVhmW4_JQ',
+    },
+    {
+      id: 9,
+      img: 'https://source.unsplash.com/07aFaTf24Kg',
+    },
+    {
+      id: 10,
+      img: 'https://source.unsplash.com/DqyYTM7pR2o',
+    },
+  ];
+
+  public imgsSelected: number[] = [];
+
+  public modalGallery:boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public onImgSelected(e: any) {
+    if (e.target.checked) {
+      this.imgsSelected.push(e.target.id);
+    } else {
+      const index = this.imgsSelected.indexOf(e.target.id);
+      this.imgsSelected.splice(index, 1);
+    }
+  }
+
+  public showImgsSelected(e: any) {
+    alert(`Qtd de imgs selecionadas ${this.imgsSelected.length}`);
+  }
+
+  public onClickImg(e: any) {
+    this.modalGallery = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
